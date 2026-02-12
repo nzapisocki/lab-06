2375ff17324da8c57865c779726cfadd8d225340
nzapisocki <nzapisoc@ualberta.ca>
2026-02-11 17:58:08 -0700

    Completed Demo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(files("C:/Users/nzapi/AppData/Local/Android/Sdk/platforms/android-36/android.jar"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.0.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.0.1")
 }
